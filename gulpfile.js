0ccc712ab9853a3f138eb801fb8f6e9edbbbc61f
Diane <diane.douglas1@gmail.com>
2016-11-18 15:15:48 -0500

    write failing test and fix location of selenium in protractor conf file.

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,39 @@
 var gulp = require('gulp');
+var protractor = require("gulp-protractor").protractor;
+var protractor_conf = require('./e2e-tests/protractor.conf');
+var browserSync = require('browser-sync');
+
+gulp.task('e2e-test', function() {
+  var bs = browserSync.create();
+  bs.init({
+    server: {
+    baseDir: "src/client"
+  },
+    open: false
+  });
+  gulp.src(protractor_conf.config.specs)
+    .pipe(protractor({
+      configFile: "e2e-tests/protractor.conf.js",
+      args: ['--baseUrl', 'http://127.0.0.1:8000']
+    }))
+    .on('error', function(e) { 
+      throw e 
+    })
+    .on('end', function () {
+      // Close browser sync server
+      bs.exit();
+    });
+});
+
+gulp.task('serve-dev', function() {
+  /**
+  * Run Browsersync with server config
+  */
+  browserSync({
+    server: "src/client",
+  });
+});
+
 gulp.task('default', function() {
   console.log('yo');
 });
